a4462af7dd5f5cf9c08267c74530f960544f5ca6
Eiki Tamashiro <s19010@std.it-college.ac.jp>
2019-05-24 14:16:31 +0900

    5/23演習

--- 0523/052305.sh
+++ 0523/052305.sh
@@ -1,2 +1,3 @@
 #find /etc -type f 2> /dev/null | xargs grep -L '172.16.40.1' 2> /dev/null
-grep -rL '172.16.40.1' /etc 2> /dev/null
+find /etc/ -type f -print0 2> /dev/null |xargs 2> grep -l '172.16.40.1' 2> /dev/null
+#grep -rL '172.16.40.1' /etc 2> /dev/null
