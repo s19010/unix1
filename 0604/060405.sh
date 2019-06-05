074d9e6846e2d4f962ffd468183b332d1de64afa
Eiki Tamashiro <s19010@std.it-college.ac.jp>
2019-06-05 12:49:25 +0900

    6/4演習

--- 0604/060405.sh
+++ 0604/060405.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function cont() {
     echo -n 'continue?(y/n)'
     read yesno
@@ -7,29 +9,18 @@ function cont() {
     fi
     echo 'contunue...'
 }
- cd $HOME/rep/unix1/
- git status
- cont
- # git add/commit/pushの選択画面
- echo 1) git add -i
- echo 2) git commit
- echo 3) git push
- echo *) exit
- echo -n select?(1/2/3/*)
- read select
- echo $select
- exit
- if [ $select = '1' ]; then
-     git add -i
-     git status
- fi
- cont
- # git add/commit/push
- echo -n 'comment: '
- read comment
- git commit -m '$comment'
- git log --oneline
- cont
- git push
- #git add
- #git commit -m 'xxx'
+cd $HOME/rep/unix1/
+git status
+cont
+git add -i
+git status
+cont
+echo -n 'comment: '
+read comment
+git commit -m '$comment'
+git log --oneline
+cont
+git push
+#git add
+#git commit -m 'xxx'
+
