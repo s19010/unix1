aefefc79401a0c27008905abec0fed044c26c50b
Eiki Tamashiro <s19010@std.it-college.ac.jp>
2019-06-05 10:18:58 +0900

    5/28演習

--- 0528/052804.sh
+++ 0528/052804.sh
@@ -1,1 +1,2 @@
- sudo du -b /var/* | sort -rn | head -n 5
+#find /var -type f | for read file; do du $file ; done | sort -nr | head -n 5
+sudo ls -lR /var | awk '{print $9, $5}' | sort -nr -k 2 | head -n 5
