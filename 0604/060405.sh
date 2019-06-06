f847110d1046e07c0d9814dab24e50c9f22864bd
Eiki Tamashiro <s19010@std.it-college.ac.jp>
2019-06-06 10:42:19 +0900

    6/4演習

--- 0604/060405.sh
+++ 0604/060405.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function cont() {
+function select() {
     echo -n 'continue?(y/n)'
     read yesno
     if [ $yesno = 'n' ]; then
@@ -11,16 +11,31 @@ function cont() {
 }
 cd $HOME/rep/unix1/
 git status
-cont
-git add -i
-git status
-cont
-echo -n 'comment: '
-read comment
-git commit -m '$comment'
-git log --oneline
-cont
-git push
-#git add
-#git commit -m 'xxx'
-
+while :
+do
+    echo '1) git add -i'
+    echo '2) git commit'
+    echo '3) git push'
+    echo '*) exit'
+    echo -n select? '(1/2/3/*) :'
+    read select
+    case "$select" in
+        1)
+            git add -i
+            git status
+            ;;
+        2)
+            echo -n comment:
+            read comment
+            git comment -m "$select"
+            git log --online
+            ;;
+        3)
+            git push
+            ;;
+        *)
+            echo "end..."
+            exit
+            ;;
+    esac
+done
