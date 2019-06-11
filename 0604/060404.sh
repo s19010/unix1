2a2d67b106e1e1db92f90d7ad6a482cb8f34e802
Eiki Tamashiro <s19010@std.it-college.ac.jp>
2019-06-11 11:48:53 +0900

    6/4演習

--- 0604/060404.sh
+++ 0604/060404.sh
@@ -2,10 +2,10 @@ if [ -d "$1" ]; then
     # ディレクトリ内のファイルが実行可能ファイルかチェックして
     # 実行可能ファイルであれば表示
     # for file in $(ls "$1")
-    for file in $(find "$1" -maxdepth 1 -tyoe f)
+    for file in $(find "$1" -maxdepth 1 -type f)
     do
         # fileが実行ファイルであれば表示
-        if [ ! -r "${1}/${file}" ] && [ ! -d "${1}/${file}" ]; then
+        if [ ! -r "${file}" ] && [ ! -d "${file}" ]; then
         # if [ -x "$file" ]; then
             # echo "$file"
             echo "$file"
